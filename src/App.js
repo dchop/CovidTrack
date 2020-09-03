a4766be34729dfabd24f650a1d5e411cd8853b08
dchop <devanshchopra@hotmail.com>
2020-09-03 14:39:18 -0700

    Created component jsx and css files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import {Cards, Chart, CountryPicker} from './components';
 
 class App extends React.Component{
     render() {
         return (
             <div>
-                <h1>App</h1>
+                <Cards />
+                <CountryPicker />
+                <Chart />
             </div>
         )
     }
 }
 
-export default Atpp;
+export default App;
