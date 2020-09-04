8110d5b97ec5f60d8e9f41657c7747fe2f390d49
dchop <devanshchopra@hotmail.com>
2020-09-04 12:49:16 -0700

    Added countries dropdown

--- src/App.js
+++ src/App.js
@@ -7,20 +7,27 @@ import {fetchData} from './api'
 class App extends React.Component{
     state = {
         data: {},
+        country: '',
     }
 
     async componentDidMount() {
         const fetchedData = await fetchData();
         
         // console.log(data);
-        this.setState({data: fetchedData});
-;    }
+        this.setState({data: fetchedData});    
+    }
+
+    handleCountryChange = async (country) => {
+        console.log(country);
+        //fetch the data
+        // set the state
+    }
     render() {
         const { data } = this.state;
         return (
             <div className={styles.containter}>
                 <Cards data={data}/>
-                <CountryPicker />
+                <CountryPicker handleCountryChange={this.handleCountryChange}/>
                 <Chart />
             </div>
         )
